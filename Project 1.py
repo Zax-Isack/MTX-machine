74ab70d8c66835b7c574747b65b8fef896483e0f
Isack Manjale <zaxisack@gmail.com>
2025-11-07 22:09:00 +0300

    Add files via upload

--- Project 1.py
+++ Project 1.py
@@ -1,28 +1,34 @@
 #_______Calculator for  simple calculation_______
 
-number1 = int(input())
-number2 = int(input())
-operations = 0
+def calculator():
+    number1 = int(input())
+    number2 = int(input())
+    operations = 0
 
-while operations < 4:
-    operations += 1
-    operation = input("OP =  ")
-
-    if operation == " +Addition ": 
-        sum = number1 + number2
-        print(sum)
-
-    elif operation == " Subtraction ": 
-        subtraction = number1 - number2
-        print(subtraction)
-
-    elif operation == " MUltiplication ":
-        product = number1 * number2
-        print(product)
-
-    elif operation == " Division "
+    while True:
         
-    else:
+        operation = input("Mathematical Operation =  ")
+        if operation == "Addition":
+            sum = number1 + number2
+            return(sum)
 
-        print("Please enter the numerical value or a selected operation in the calculator.")
+        elif operation == "Subtraction":
+            subtraction = number1 - number2
+            return(subtraction)
+
+        elif operation == "Multiplication":
+            product = number1 * number2
+            return(product)
+
+        elif operation == "Division":
+            division = number1 / number2
+            return(division)
+
+        else:
+            return("Please enter the numerical value or a selected operation in the calculator.")
+
+
+calculations = calculator()
+print(calculations)
+
 
